2d7261b0a35df162d42fff8e67053f1c4a6f4890
franklinls <franklinls@neowrk.com>
2025-02-12 17:11:06 -0300

    fix: energy consumption formula for inferencing with half cores

--- src/InferenceBenckmark/monitor/consumptionmetrics.py
+++ src/InferenceBenckmark/monitor/consumptionmetrics.py
@@ -1,5 +1,6 @@
 import psutil
 
+
 class ConsumptionMetrics:
     __cpu_usage_levels: list = list()
     __cpu_temperature_levels: list = list()
@@ -13,23 +14,36 @@ class ConsumptionMetrics:
     @staticmethod
     def update() -> None:
         with open("/sys/class/thermal/thermal_zone0/temp", "r") as file:
-            ConsumptionMetrics.__cpu_temperature_levels.append(float(file.read()) / 1000.0)
+            ConsumptionMetrics.__cpu_temperature_levels.append(
+                float(file.read()) / 1000.0
+            )
         ConsumptionMetrics.__cpu_usage_levels.append(psutil.cpu_percent())
-        ConsumptionMetrics.__ram_usage_levels.append(psutil.virtual_memory().used / (1024 * 1024))
-    
-    @staticmethod
-    def compute_current_levels(min_current: int, max_current: int) -> None:
-        ConsumptionMetrics.__current_usage_levels = [
-            min_current + (max_current - min_current) * (cpu_usage / 100) for cpu_usage in ConsumptionMetrics.__cpu_usage_levels
-        ]
+        ConsumptionMetrics.__ram_usage_levels.append(
+            psutil.virtual_memory().used / (1024 * 1024)
+        )
 
+    @staticmethod
+    def compute_current_levels(
+        min_current: int, max_current: int, half_cores: bool
+    ) -> None:
+        if not half_cores:
+            cpu_max = 100
+        else:
+            cpu_max = 50
+
+        ConsumptionMetrics.__current_usage_levels = [
+            min_current + (max_current - min_current) * (cpu_usage / cpu_max)
+            for cpu_usage in ConsumptionMetrics.__cpu_usage_levels
+        ]
 
     @staticmethod
     def get_measures() -> list:
         if ConsumptionMetrics.__cpu_usage_levels:
-            return ConsumptionMetrics.__cpu_usage_levels, ConsumptionMetrics.__cpu_temperature_levels, ConsumptionMetrics.__ram_usage_levels, ConsumptionMetrics.__current_usage_levels
+            return (
+                ConsumptionMetrics.__cpu_usage_levels,
+                ConsumptionMetrics.__cpu_temperature_levels,
+                ConsumptionMetrics.__ram_usage_levels,
+                ConsumptionMetrics.__current_usage_levels,
+            )
         else:
-            return None 
-        
-    
-
+            return None
